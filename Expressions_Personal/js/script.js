1d793caf57b3733a4382817174becbd8e635b461
Vanessa Marrero <msvmarrero@gmail.com>
2014-05-15 21:51:18 -0400

    JS Greeting and Name Prompt
    Inserted a welcome message and a prompt asking the user for their name.
    
    Also reporting the name of the user to the console.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,18 @@ Vanessa Marrero
 May 15, 2014
 Expressions Assignment
 */
+
+// This is the initial welcome message users will receive.
+var greeting = "Hello! \n\nWelcome to my personal calculator!";
+
+// This is the alert with the welcome message.
+alert(greeting);
+
+// This is the message requesting the user's name.
+var nameRequest = "Before we begin, what is your name?";
+
+// This is capturing the user's name from the prompt in a new variable.
+var name = prompt(nameRequest); 
+
+//This is reporting the user's name to the console. 
+console.log("The user\'s name is " + name + ".");
