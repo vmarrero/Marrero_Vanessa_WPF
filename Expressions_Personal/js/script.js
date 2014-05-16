835c42fe429002454d735ab8176f3cc590b7e656
Vanessa Marrero <msvmarrero@gmail.com>
2014-05-15 21:59:43 -0400

    Explanation and Calculation
    This explains to the user what the calculator is for.
    
    Also, includes a prompt and capturing that prompt into a variable in
    order to perform a calculation.
    
    Reports the number of beer bottles consumed by the user per year via an
    alert and also reports it to the console.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -18,3 +18,24 @@ var name = prompt(nameRequest);
 
 //This is reporting the user's name to the console. 
 console.log("The user\'s name is " + name + ".");
+
+// This is explaining to the user what it is we are calculating.
+var explanation = "Thank you " + name + "! \n\nToday, we will be calculating how many bottles of beer you consume in a year and how much it costs you per year."; 
+
+// This is the alert with the explanation.
+alert(explanation); 
+
+// This is the message asking for the amount of beer bottles consumed in a week.
+var beerBottles = "How many beers do you consume a week?";
+
+// This is capturing the number of beer bottles the user drinks in a week.
+var weeklyBeerBottlesNumber = prompt(beerBottles); 
+
+// This is calculating how many bottles of beer the user drinks a year based off the number they drink a week.
+var yearlyBeerBottlesNumber = weeklyBeerBottlesNumber * 52;
+
+// This is an alert informing the user how many beer bottless they consume per year.
+alert(name + ", you consume " + yearlyBeerBottlesNumber + " bottles of beer a year. \n\nCheers!");
+
+// This is reporting the number of beer bottles the user drinks per year to the console.  
+console.log(name + " consumes " + yearlyBeerBottlesNumber + " bottles of beer a year.");
