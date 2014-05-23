8565c0ac3757ccea06246ad0cefce6e39527a4f3
Vanessa Marrero <msvmarrero@gmail.com>
2014-05-23 00:28:38 -0400

    Name to Console and Explanation
    Reports user’s name to the console and provides explanation of
    calculation to be executed.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -27,3 +27,12 @@ while (!isNaN(name) || name === null || name === "!" || name === "@" || name ===
     alert("You need to input letters for your name!");
     var name = prompt("Please enter your name using letters.");
 }
+
+//This reports the user's name to the console. 
+console.log("The user's name is " + name + ".");
+
+// This explains to the user what it is we are calculating.
+var explanation = "Thank you " + name + "! \n\nToday, we will be calculating how much you weigh on each of the planets in our solar system as well as how old you are on those planets.";
+
+// This is the alert with the calculation explanation.
+alert(explanation);
