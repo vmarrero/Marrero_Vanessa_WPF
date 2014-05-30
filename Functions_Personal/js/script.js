7ecb9181bbd345e3094bde85a06008341ea7f792
Vanessa Marrero <msvmarrero@gmail.com>
2014-05-29 23:00:15 -0400

    JS Greeting and Name Prompt
    JS Greeting and Name Prompt

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,3 +3,15 @@ Vanessa Marrero
 May 29, 2014
 Functions Assignment
 */
+
+// This is the welcome message users receive.
+var greet = "Hello! \n\nWelcome to my personal calculator!";
+
+// This is the alert with the welcome message.
+alert(greet);
+
+// This is the message asking for the user's name.
+var requestName = "First off, what is your name?";
+
+// This is storing the user's name from the prompt into a new variable.
+var n = prompt(requestName);
