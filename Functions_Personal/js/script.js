d37a25e9b49d65ebda7daadaa0d99f05e3ba7f77
Vanessa Marrero <msvmarrero@gmail.com>
2014-05-29 23:04:28 -0400

    Calculation Explanation
    Provides explanation of calculation to be executed.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -21,3 +21,15 @@ while (!isNaN(n) || n < 1 || n === "!" || n === "@" || n === "#" || n === "$" ||
     alert("You need to put LETTERS for your name!");
     var n = prompt("Enter your name using LETTERS!");
 }
+
+// This explains to the user what we are calculating.
+var explain = "Thank you " + n + "! \n\nWe're going to be calculating your body mass index (BMI) in order to figure out under which classification your weight falls into - Underweight, Normal, Overweight, or Obese.";
+
+// This is the alert with the calculator explanation.
+alert(explain);
+
+// This explains to the user what is body mass index.
+var bmiExplain = n + ", a person's body mass index is a measure of body fat based on an individual's height and weight. \n\nDoctors use BMI to determine the healthiness of an individual's weight.";
+
+// This is the alert with the explanation of bmi.
+alert(bmiExplain);
